37a0c20ef6afec8d3b07fb9234b88f34692e375a
Azerus96 <gencinski1996@gmail.com>
2024-09-12 16:47:25 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# src/__init__.py
+
+# Этот файл может быть пустым, но если нужно, можно исполнять начальную
+# инициализацию пакета здесь. Например, импортировать чаще используемые 
+# компоненты пакета для упрощенного доступа.
+
+# from .poker_bot import PokerBot
+# from .game_logic import PokerGame
+# from .browser_automation import BrowserAutomation
